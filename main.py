c5fe42014692257a8d998476126d65bc463a4c79
Benjmanxd <benjaminchoi00@gmail.com>
2023-04-23 03:07:42 +0800

    added functions to update

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class Root(BoxLayout):
+    
     def __init__(self):
         super(Root, self).__init__()
+    
+    def download_files(self):
+        print("downloading")
+
     def update_download_text(self):
         self.download_label.text = "hihi";
+
+    def update(self):
+        self.update_download_text()
+        print("updated")
+
 
 class YoutubeDownloader(App):
     def build(self):
